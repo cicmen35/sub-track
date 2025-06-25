6d1215a4b8e8bce8b8a02da8d698f9a558f76704
simonnemcic <simonnemci35@gmail.com>
2025-06-25 11:57:15 +0200

    fix: didnt commit the app.js last time

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import { PORT } from './config/env.js';
 
@@ -9,6 +10,10 @@ import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js'
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
